063d7ca15f6362e3e7dbd4586f8c6c4cbff73a08
JR Studio <jrstudio8087@gmail.com>
2023-07-05 17:10:38 +0200

    Iniciando proyecto GAIA

---
+++ src/main/java/com/jrstudio/gaia/GaiaApplication.java
@@ -0,0 +1,13 @@
+package com.jrstudio.gaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrstudio/gaia/GaiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrstudio.gaia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
